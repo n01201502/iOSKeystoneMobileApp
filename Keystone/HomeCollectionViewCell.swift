9e9360b5781775f7d5d25eabd27a0f79d742a39a
pineshmenat@gmail.com <pineshmenat.gmail.com>
2018-04-04 17:01:18 -0400

    HomeScreen Added

---
+++ Keystone/HomeCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  HomeCollectionViewCell.swift
+//  Keystone
+//
+//  Created by Adit Dhruv on 2018-04-04.
+//  Copyright © 2018 Softchoice. All rights reserved.
+//
+
+import UIKit
+
+class HomeCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var imageViewCell: UIImageView!
+}
---
+++ Keystone/HomeViewController.swift
@@ -0,0 +1,46 @@
+//
+//  HomeViewController.swift
+//  Keystone
+//
+//  Created by Adit Dhruv on 2018-04-04.
+//  Copyright © 2018 Softchoice. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController,UICollectionViewDataSource{
+    @IBOutlet weak var homeCollection: UICollectionView!
+    
+    let array:[String] = ["user", "camera", "inventory", "list", "add", "call"]
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let itemSize = UIScreen.main.bounds.width/2 - 1
+        let itemSizeHeight = UIScreen.main.bounds.height/5 - 1
+        
+        let layout = UICollectionViewFlowLayout()
+        layout.itemSize = CGSize(width: itemSize, height: itemSizeHeight)
+        
+        layout.minimumInteritemSpacing = 2
+        layout.minimumLineSpacing = 2
+        
+        homeCollection.collectionViewLayout = layout
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    //Number of views
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return array.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! HomeCollectionViewCell
+        cell.imageViewCell.image = UIImage(named: array[indexPath.row] + ".png")
+        return cell
+    }
+}
