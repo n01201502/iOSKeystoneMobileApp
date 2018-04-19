7d8c7a0680ada9783e20c29b6732dad2d47c4e2c
Menat Pinesh I <n01205889@nl-00205-017.local>
2018-04-19 01:02:03 -0400

    postHTTP returning json

--- Keystone/ApiHttpPostUtil.swift
+++ Keystone/ApiHttpPostUtil.swift
@@ -9,29 +9,25 @@
 import Foundation
 
 class ApiHttpPostUtil {
-    var dataString:NSString = ""
     
-    public func postHttp(bodyParamenters: String) -> NSString{
-        
-        func dataRequest() {
-            let urlToRequest = "http://keystone.appsolution4u.com/keystone_android/keystoneWebService.php"
-            let url4 = URL(string: urlToRequest)!
-            let session4 = URLSession.shared
-            let request = NSMutableURLRequest(url: url4)
-            request.httpMethod = "POST"
-            request.cachePolicy = NSURLRequest.CachePolicy.reloadIgnoringCacheData
-            request.httpBody = bodyParamenters.data(using: String.Encoding.utf8)
-            let task = session4.dataTask(with: request as URLRequest) { (data, response, error) in
-                guard let _: Data = data, let _: URLResponse = response, error == nil else {
-                    print("*****error")
-                    return
-                }
-                self.dataString = NSString(data: data!, encoding: String.Encoding.utf8.rawValue)!
-                //print(self.dataString)
+    public func postHttp(bodyParamenters: String, completion: @escaping (NSString) -> ()){
+        var dataString:NSString = ""
+        let urlToRequest = "http://keystone.appsolution4u.com/keystone_android/keystoneWebService.php"
+        let url4 = URL(string: urlToRequest)!
+        let session4 = URLSession.shared
+        let request = NSMutableURLRequest(url: url4)
+        request.httpMethod = "POST"
+        request.cachePolicy = NSURLRequest.CachePolicy.reloadIgnoringCacheData
+        request.httpBody = bodyParamenters.data(using: String.Encoding.utf8)
+        let task = session4.dataTask(with: request as URLRequest)
+        { data, response, error in
+            guard let data = data, error == nil else {                                                 // check for fundamental networking error
+                completion(dataString)
+                return
             }
-            task.resume()
+            dataString = NSString(data: data, encoding: String.Encoding.utf8.rawValue)!
+            completion(dataString)
         }
-        dataRequest()
-        return self.dataString
+        task.resume()
     }
 }
--- Keystone/TicketViewController.swift
+++ Keystone/TicketViewController.swift
@@ -2,93 +2,40 @@
 //  TicketViewController.swift
 //  Keystone
 //
-//  Created by Adit Dhruv on 2018-04-18.
+//  Created by Menat Pinesh I. on 4/18/18.
 //  Copyright © 2018 Softchoice. All rights reserved.
 //
 
 import UIKit
 
-class TicketViewController: UITableViewController {
+class TicketViewController: UIViewController,UITableViewDelegate,UITableViewDataSource {
 
+    @IBOutlet weak var TicketView: UITableView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
         
         let apiPostHttp = ApiHttpPostUtil()
-        let data = apiPostHttp.postHttp(bodyParamenters: "operation=getTickets&company_id=1001")
-        print(data)
+        apiPostHttp.postHttp(bodyParamenters: "operation=getTickets&company_id=1001"){ code in
+            print(code)
+        }
     }
     
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-    // MARK: - Table view data source
-
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 1
-    }
-
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 1
-    }
-
-    
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "ticket", for: indexPath)
-
+        
         // Configure the cell...
-
+        
         return cell
     }
     
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
     }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
     }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
