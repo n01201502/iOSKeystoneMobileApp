273eab5ad176e018a51e21de927106ec44c801f1
pineshmenat@gmail.com <pineshmenat.gmail.com>
2018-04-20 05:32:34 -0400

    Tickets Objects Created from JSON

--- Keystone/Ticket.swift
+++ Keystone/Ticket.swift
@@ -9,40 +9,56 @@
 import Foundation
 import ObjectMapper
 
-class Ticket:Mappable{
-    var ticket_id: Int?
-    var incident_type: Int?
+class Ticket{
+    var ticket_id: String?
+    var incident_type: String?
     var ticket_type: String?
     var ticket_priority: String?
     var ticket_description: String?
-    var company_id: Int?
+    var company_id: String?
     var ticket_class: String?
     var ticket_status: String?
     var ticket_reason: String?
     var ticket_requester: String?
     var ticket_category: String?
-    var ticket_created_datetime: Date?
-    var device_id: Int?
+    var ticket_created_datetime: String?
+    var device_id: String?
     
-    
-    required init?(map: Map) {
-        
+    init (ticket_id:String,incident_type:String,ticket_type:String,ticket_priority: String, ticket_description: String,company_id: String,ticket_class: String,
+          ticket_status:String,ticket_reason:String,ticket_requester:String,ticket_category: String,ticket_created_datetime: String,device_id: String) {
+        self.ticket_id = ticket_id
+        self.incident_type = incident_type
+        self.ticket_type = ticket_type
+        self.ticket_priority = ticket_priority
+        self.ticket_description = ticket_description
+        self.company_id = company_id
+        self.ticket_class = ticket_class
+        self.ticket_status = ticket_status
+        self.ticket_reason = ticket_reason
+        self.ticket_requester = ticket_requester
+        self.ticket_category = ticket_category
+        self.ticket_created_datetime = ticket_created_datetime
+        self.device_id = device_id
     }
-    
-    // Mappable
-    func mapping(map: Map) {
-        ticket_id               <- map["ticket_id"]
-        incident_type           <- map["incident_type"]
-        ticket_type             <- map["ticket_type"]
-        ticket_priority         <- map["ticket_priority"]
-        ticket_description      <- map["ticket_description"]
-        company_id              <- map["company_id"]
-        ticket_class            <- map["ticket_class"]
-        ticket_status           <- map["ticket_status"]
-        ticket_reason           <- map["ticket_reason"]
-        ticket_requester        <- map["ticket_requester"]
-        ticket_category         <- map["ticket_category"]
-        ticket_created_datetime <- (map["ticket_created_datetime"], DateTransform())
-        device_id               <- map["device_id"]
-    }
+//
+//    required convenience init?(map: Map) {
+//        self.init()
+//    }
+//
+//    // Mappable
+//    func mapping(map: Map) {
+//        ticket_id               <- map["ticket_id"]
+//        incident_type           <- map["incident_type"]
+//        ticket_type             <- map["ticket_type"]
+//        ticket_priority         <- map["ticket_priority"]
+//        ticket_description      <- map["ticket_description"]
+//        company_id              <- map["company_id"]
+//        ticket_class            <- map["ticket_class"]
+//        ticket_status           <- map["ticket_status"]
+//        ticket_reason           <- map["ticket_reason"]
+//        ticket_requester        <- map["ticket_requester"]
+//        ticket_category         <- map["ticket_category"]
+//        ticket_created_datetime <- (map["ticket_created_datetime"], DateTransform())
+//        device_id               <- map["device_id"]
+//    }
 }
--- Keystone/TicketViewController.swift
+++ Keystone/TicketViewController.swift
@@ -11,26 +11,12 @@ import Alamofire
 import ObjectMapper
 
 class TicketViewController: UIViewController,UITableViewDelegate,UITableViewDataSource {
-
-    @IBOutlet weak var TicketView: UITableView!
     
+    @IBOutlet weak var TicketView: UITableView!
+    var tickets = [Ticket]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-//        let apiPostHttp = ApiHttpPostUtil()
-//        apiPostHttp.postHttp(bodyParamenters: "operation=getTickets&company_id=1001"){ code in
-//            print(code)
-//        }
-        
-//        Alamofire.request("http://keystone.appsolution4u.com/keystone_android/keystoneWebService.php?operation=getTickets&company_id=1001", method: .post).validate().responseJSON { response in
-//            switch response.result {
-//            case .success:
-//                print("Validation Successful")
-//            case .failure(let error):
-//                print(error)
-//            }
-//        }
         
         let parameters:Parameters = [
             "operation":"getTickets",
@@ -39,11 +25,10 @@ class TicketViewController: UIViewController,UITableViewDelegate,UITableViewData
         
         Alamofire.request("http://keystone.appsolution4u.com/keystone_android/keystoneWebService.php", method: .post, parameters: parameters)
             .responseJSON { response in
-                print(response)
                 //to get status code
                 if let status = response.response?.statusCode {
                     switch(status){
-                    case 201:
+                    case 200:
                         print("example success")
                     default:
                         print("error with response status: \(status)")
@@ -51,11 +36,42 @@ class TicketViewController: UIViewController,UITableViewDelegate,UITableViewData
                 }
                 //to get JSON return value
                 if let result = response.result.value {
-                    let JSON = result as! NSDictionary
-                    print(JSON)
+                    if let JSON = result as? NSDictionary {
+                        if let data = JSON["success"] {
+                            if(data as! Bool == true) {
+                                //print(data)
+                                if let dataarray = JSON["data"] {
+                                    //print(dataarray)
+                                    for listTicket in (dataarray as? NSArray)! {
+                                        //print(singleTicket)
+                                        if let singleTicket = listTicket as? [String: Any] {
+                                            self.tickets.append(Ticket(ticket_id:singleTicket["ticket_id"] as! String,
+                                                                  incident_type: singleTicket["incident_type"] as! String,
+                                                                  ticket_type: singleTicket["ticket_type"] as! String,
+                                                                  ticket_priority: singleTicket["ticket_priority"] as! String,
+                                                                  ticket_description: singleTicket["ticket_description"] as! String,
+                                                                  company_id: singleTicket["company_id"] as! String,
+                                                                  ticket_class: singleTicket["ticket_class"] as! String,
+                                                                  ticket_status: singleTicket["ticket_status"] as! String,
+                                                                  ticket_reason: singleTicket["ticket_reason"] as! String,
+                                                                  ticket_requester: singleTicket["ticket_requester"] as! String,
+                                                                  ticket_category: singleTicket["ticket_category"] as! String,
+                                                                  ticket_created_datetime: singleTicket["ticket_created_datetime"] as! String,
+                                                                  device_id: singleTicket["device_id"] as! String
+                                                                  ))
+                                            //print(self.tickets.count)
+                                        }
+                                        //print("------------------------------")
+                                    }
+                                }
+                            }
+                        }
+                    }
+                    
                 }
                 
         }
+        
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
@@ -73,5 +89,5 @@ class TicketViewController: UIViewController,UITableViewDelegate,UITableViewData
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return 1
     }
-
+    
 }
